812914647a5ce2eed97c7b22f22625f83d046047
muratdrmz <mrtdrmz@windowslive.com>
2023-01-21 19:44:06 +0100

    19:44

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const ratingEls=document.querySelectorAll('.rating');
+const btnEl=document.getElementById('btn');
+const containerEl=document.getElementById('container');
+
+let selectedRaiting='';
 
 ratingEls.forEach((ratingEl)=>{
  ratingEl.addEventListener('click',function(e){
@@ -7,8 +11,22 @@ ratingEls.forEach((ratingEl)=>{
   e.target.classList.add('active');
   e.target.parentNode.classList.add('active')
 
+  selectedRaiting=e.target.innerText || e.target.parentNode.innerText;
+
  });
 
+});
+
+btnEl.addEventListener('click',()=>{
+ if(selectedRaiting!==''){
+  containerEl.innerHTML=`
+  <strong>Thank you!</strong>
+  <br>
+  <br>
+  <strong>Feedback:${selectedRaiting}</strong>
+  <p>We'll use your feedback to improve our customer support.</p>
+  `
+ }
 });
 
 function removeActive (){
